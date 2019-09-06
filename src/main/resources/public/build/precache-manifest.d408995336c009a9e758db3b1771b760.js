853a3b2b29f0985ccc1891a9df42814b33d7084e
赵越海 <zhaoyuehai5282@163.com>
2019-09-06 20:10:45 +0800

    前端react

---
+++ src/main/resources/public/build/precache-manifest.d408995336c009a9e758db3b1771b760.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9bd630d130ed62d3960b8afe2fb2de5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4eec469876643f1d8cec",
+    "url": "./static/css/main.9cbc6d98.chunk.css"
+  },
+  {
+    "revision": "21c68accd71a75e97afc",
+    "url": "./static/js/2.a971823f.chunk.js"
+  },
+  {
+    "revision": "4eec469876643f1d8cec",
+    "url": "./static/js/main.5ba5173f.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
